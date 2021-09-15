91d176a5e0a91c46af5696a810ebced507542287
Oxyjon <47044069+Oxyjon@users.noreply.github.com>
2021-09-16 08:46:23 +1000

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from morse_code import morse_code_rules
+
+word = input("What word would you like to convert to morse code?")
+
+#Method 1: #inputs into string
+# converted_word = ''
+#
+# for char in word:
+#     converted_word += morse_code_rules[char]
+# print(converted_word)
+
+#Method 2: #inputs into list
+# converted_word = [''.join(morse_code_rules[char]) for char in word]
+#
+# print(converted_word)
+
+#Method 3: #inputs into list then into a string
+converted_word = [''.join(morse_code_rules[char]) for char in word]
+endstring = ''
+for code in converted_word:
+    endstring += code + ' '
+
+print(endstring)
